9a6a5236f70fb621f5f559b51fe45763daed5fe5
Aash7710 <aashpatel7710@gmail.com>
2024-08-07 01:29:00 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from  flask import Flask, render_template, request
+'''from flask import Flask, render_template, request, redirect, url_for, session
 import pickle
 import numpy as np
 
 popular_df = pickle.load(open('popular.pkl', 'rb'))
-pt = pickle.load(open('pt.pkl','rb'))
-books = pickle.load(open('books.pkl','rb'))
-similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
+options = pickle.load(open('options.csv', 'rb'))
 
 app = Flask(__name__)
 
@@ -21,7 +22,8 @@ def index():
 
 @app.route('/recommend')
 def recommend_ui():
-    return render_template('recommend.html')
+    movies = options['Movie_name']
+    return render_template('recommend.html', movies=movies)
 
 @app.route('/recommend_books', methods=['post'])
 def recommend():
@@ -44,5 +46,60 @@ def recommend():
     return render_template('recommend.html', data=data)
 
 
+
+if __name__ == '__main__':
+    app.run(debug=True)'''
+
+from flask import Flask, render_template, request, redirect, url_for, session
+import pickle
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
+options = pickle.load(open('options.csv', 'rb'))
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Required for using sessions
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num_rating'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           )
+
+@app.route('/recommend')
+def recommend_ui():
+    movies = options['Movie_name']
+    data = session.get('recommendations', None)  # Retrieve recommendations from the session
+    return render_template('recommend.html', movies=movies, data=data)
+
+@app.route('/recommend_books', methods=['POST'])
+def recommend():
+    user_input = request.form.get('user_input')
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(
+    similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:9]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+
+    session['recommendations'] = data  # Store recommendations in the session
+    return redirect(url_for('recommend_ui'))  # Redirect to the recommend page
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
